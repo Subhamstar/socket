b5f77dbfc2ad6cab7bfef0a0395206eb73a5e92d
Subhamstar <mr.subhamdas999@gmail.com>
2025-11-17 21:04:14 +0530

    making 1 way

--- index.js
+++ index.js
@@ -10,13 +10,24 @@ const io=socketIo(server);
 io.on('connection',(socket)=>{
     console.log("new client connected "+socket.id);
 
-    socket.on('from-client',()=>{
-        console.log("Event coming from client")
+
+
+
+
+    socket.on('msg_send',(data)=>{
+        console.log(data);
+        io.emit('msg_rcvd',data);    // display to all including itself
+        // socket.emit('msg_rcvd',data);     // only display its own message
+        // socket.broadcast.emit('msg_rcvd',data);       // display to all except itself
     })
 
-    setInterval(()=>{
-        socket.emit('from-server',new Date())
-    },2000);
+    // socket.on('from_client',()=>{
+    //     console.log("Event coming from client")
+    // })
+
+    // setInterval(()=>{
+    //     socket.emit('from-server',new Date())
+    // },2000);
 })
 
 app.use('/',express.static(__dirname+'/public'));
--- public/script.js
+++ public/script.js
@@ -1,14 +1,30 @@
 var socket=io();
 
 let btn=document.getElementById('btn');
-btn.addEventListener('click',()=>{
-    socket.emit("from-client")
+let newmsg=document.getElementById('newmsg');
+let msglist=document.getElementById('msglist');
+
+btn.onclick=function exec(){
+    socket.emit('msg_send',{
+        msg:newmsg.value 
+    })
+}
+socket.on("msg_rcvd",(data)=>{
+    let limsg=document.createElement('li');
+    limsg.innerText=data.msg;
+    msglist.appendChild(limsg);
 })
 
-socket.on('from-server',()=>{
-    console.log("Collected a new event from server");
-    const div=document.createElement('div');
-    div.innerText='New Event from server';
-    // console.log(div); 
-    document.body.appendChild(div);
-})
+
+
+// btn.addEventListener('click',()=>{
+//     socket.emit("from-client")
+// })
+
+// socket.on('from-server',()=>{
+//     console.log("Collected a new event from server");
+//     const div=document.createElement('div');
+//     div.innerText='New Event from server';
+//     // console.log(div); 
+//     document.body.appendChild(div);
+// })
